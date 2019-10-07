ad6a1a62e582a16199785e5997338c9c8bdf7b7c
xixi-0314 <2777083653@qq.com>
2019-10-07 17:43:28 +0800

    包的测试

---
+++ PackageTest/src/MainClass.java
@@ -0,0 +1,11 @@
+import course.*;
+import student.*;
+import teacher.*;
+
+public class MainClass{
+         public static void main (String [] args){
+                  Student st =new Student();
+                  Teacher te =new Teacher();
+                  Course  co =new Course();
+        }
+}
